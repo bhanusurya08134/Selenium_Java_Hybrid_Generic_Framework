d394f99e525dff770b76d4c4205db07616932e60
Bhanu Surya <bhanu.surya@global-csg.com>
2024-12-12 10:27:06 +0530

    adding cucumber json file

--- src/test/java/ui/creative/testrunners/DWS_UI_Runner_INT001.java
+++ src/test/java/ui/creative/testrunners/DWS_UI_Runner_INT001.java
@@ -14,7 +14,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(features = "src/test/java/ui/creative/features",tags= "@DWS_01",glue = "ui/creative/stepdefinitions", monochrome = true,plugin = { "html:target/cucumber.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+@CucumberOptions(publish = true, features = "src/test/java/ui/creative/features",tags= "@DWS_01",glue = "ui/creative/stepdefinitions", monochrome = true,plugin = { "json:target/cucumber.json", "html:target/cucumber.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class DWS_UI_Runner_INT001 extends AbstractTestNGCucumberTests{
 	@BeforeClass
 	public void beforeCalss() throws AWTException {
